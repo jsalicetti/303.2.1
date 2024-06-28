1c49b0c9719acea99dd2067890afa05f4de277e5
jsalicetti <jessicasalicetti@yahoo.com>
2024-06-28 10:47:01 -0400

    integer & double variables

---
+++ LAB303.2.1/src/intDoubleVariable.java
@@ -0,0 +1,18 @@
+public class intDoubleVariable {
+    public static class SumIntAndDouble {
+        public static void main(String[] args) {
+            // Declare an integer variable
+            int num1 = 10;
+
+            // Declare a double variable
+            double num2 = 20.5;
+
+            // Calculate the sum (int + double will result in a double)
+            double sum = num1 + num2;
+
+            // Print out the result
+            System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        }
+    }
+
+}
