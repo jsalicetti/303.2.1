6f5607218f1d19e7f484dd8a24a0e21a5dd840d4
jsalicetti <jessicasalicetti@yahoo.com>
2024-06-28 10:43:07 -0400

    double variables

--- LAB303.2.1/src/doubleVariables.java
+++ LAB303.2.1/src/doubleVariables.java
@@ -1,12 +1,12 @@
-public class sumOfIntegers {
-    public static class SumOfIntegers {
+public class doubleVariables {
+    public static class SumOfDoubles {
         public static void main(String[] args) {
-            // Declare two integer variables
-            int num1 = 10;
-            int num2 = 20;
+            // Declare two double variables
+            double num1 = 10.5;
+            double num2 = 20.3;
 
             // Calculate the sum
-            int sum = num1 + num2;
+            double sum = num1 + num2;
 
             // Print out the result
             System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
@@ -14,5 +14,3 @@ public class sumOfIntegers {
     }
 
 }
-
-
