36ce440bc69447933c31bb052a1030ef128a0fdf
jsalicetti <jessicasalicetti@yahoo.com>
2024-06-28 10:40:58 -0400

    two integers

---
+++ LAB303.2.1/src/sumOfIntegers.java
@@ -0,0 +1,16 @@
+public class sumOfIntegers {
+    public static class SumOfIntegers {
+        public static void main(String[] args) {
+            // Declare two integer variables
+            int num1 = 10;
+            int num2 = 20;
+
+            // Calculate the sum
+            int sum = num1 + num2;
+
+            // Print out the result
+            System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        }
+    }
+
+}
