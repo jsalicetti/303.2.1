74d7e98ebf01895a95a7f29645c704170d752cb8
jsalicetti <jessicasalicetti@yahoo.com>
2024-06-28 11:40:35 -0400

    const

---
+++ LAB303.2.1/src/constCalc.java
@@ -0,0 +1,15 @@
+public class constCalc {
+    public class ConstantExample {
+        public static void main(String[] args) {
+            // Declare a named constant
+            final int CONSTANT_VALUE = 10;
+
+            // Use the constant in a calculation
+            int result = CONSTANT_VALUE * 5;
+
+            // Print the result
+            System.out.println("Result of using constant in calculation: " + result);
+        }
+    }
+
+}
